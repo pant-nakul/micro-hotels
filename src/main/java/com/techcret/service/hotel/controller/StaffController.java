70a61ce3fca02e2518546adb891d8d4dd0f19414
Nakul Ashish Pant <nakul.paradox@gmail.com>
2025-02-26 02:58:48 +0530

    Added staff controller

---
+++ src/main/java/com/techcret/service/hotel/controller/StaffController.java
@@ -0,0 +1,21 @@
+package com.techcret.service.hotel.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/staffs")
+public class StaffController {
+
+
+    @GetMapping("/")
+    public ResponseEntity<List<String>> getAllStaffs(){
+        return ResponseEntity.ok().body(Arrays.asList("Ram","Krishna","ParshuRam","Shiva"));
+    }
+
+}
